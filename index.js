a489efeaf16585be64492eb654ff281c8009cab8
s2045318 <s2045318@ed.ac.uk>
2023-03-03 11:21:17 +0200

    4.1

---
+++ index.js
@@ -0,0 +1,53 @@
+const http = require('http')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+//const config = require('./utils/config')
+const mongoose = require('mongoose')
+mongoose.set('strictQuery', false)
+
+require('dotenv').config()
+
+const PORT = process.env.PORT
+const url = process.env.MONGODB_URI
+
+
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
+
+
+
+
+const Blog = mongoose.model('Blog', blogSchema)
+mongoose.connect(url)
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/api/blogs', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+})
+
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
---
+++ utils/config.js
@@ -0,0 +1,7 @@
+
+const mongoUrl = 'mongodb+srv://s2045318:39HawthornRoad*@cluster0.tf3kzpl.mongodb.net/?retryWrites=true&w=majority'
+const PORT = 3001
+
+module.exports = {
+    mongoUrl, PORT
+}
