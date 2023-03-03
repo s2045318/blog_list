f0c5f16874fd6931e16097ebe6798e90ba0031ef
s2045318 <s2045318@ed.ac.uk>
2023-03-03 20:23:34 +0200

    4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,16 @@
 const dummy = (blogs) => {
     return 1
   }
+
+const totalLikes = (blogs) => {
+    const sum = blogs.reduce(
+        (accumulator, currentValue) => accumulator + currentValue.likes,
+        0
+      )
+    console.log(sum)
+    return sum
+}
   
-  module.exports = {
-    dummy
-  }
+module.exports = {
+    dummy, totalLikes
+}
