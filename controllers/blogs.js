1145a7cc4ef16480082fbffa70126b09a1995ff1
s2045318 <s2045318@ed.ac.uk>
2023-03-03 19:59:40 +0200

    4.2

---
+++ controllers/blogs.js
@@ -0,0 +1,30 @@
+const blogRouter = require('express').Router()
+const Blog = require('../models/blogs')
+
+  
+blogRouter.get('/', (request, response, next) => {
+Blog
+    .find({})
+    .then(blogs => {
+        response.json(blogs)
+    })
+    
+})
+
+blogRouter.post('/', (request, response, next) => {
+    const body = request.body
+    const blog = new Blog({
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes    
+    })
+    blog
+        .save()
+        .then(result => {
+          response.status(201).json(result)
+        })
+        .catch(error => next(error))
+    })
+
+module.exports = blogRouter
